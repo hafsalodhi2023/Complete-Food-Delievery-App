00610f79583b5a0a4b6126ce031cd1b84abdf581
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2024-03-23 12:38:00 +0500

    Created Navbar

---
+++ src/screens/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div>Body</div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+}
